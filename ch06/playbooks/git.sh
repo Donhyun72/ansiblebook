2c3b355618ba203f9c7049217a01bb48f74b0107
samuel-c <samuel.c@kakaocommerce.com>
2019-01-28 03:56:58 +0900

    나머지 정리.

--- ch06/playbooks/git.sh
+++ ch06/playbooks/git.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible web -m git -a "repo=git@github.com:lorin/mezzanine-example.git dest=/home/vagrant/mezzanine-example/project accept_hostkey=yes"
+ansible web -m git -a "repo=git@github.com:knight76/mezzanine-example.git dest=/home/vagrant/mezzanine-example/project accept_hostkey=yes"
